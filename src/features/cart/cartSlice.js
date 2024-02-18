ebf92eb4f2fcd1c5f310a30b7aa91a09669aa43c
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-18 15:16:02 +0330

    Redux Functions

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -12,11 +12,38 @@ const cartSlice = createSlice({
         state.push({ ...action.payload, quantity: 1 });
       }
     },
+    removeItemFromCart: (state, action) => {
+      const index = state.findIndex((item) => item.id === action.payload);
+      if (index !== -1) {
+        state.splice(index, 1);
+      }
+    },
+    incrementQuantity: (state, action) => {
+      const index = state.findIndex((item) => item.id === action.payload);
+      if (index !== -1) {
+        state[index].quantity += 1;
+      }
+    },
+
+    decrementQuantity: (state, action) => {
+      const index = state.findIndex((item) => item.id === action.payload);
+      if (index !== -1) {
+        state[index].quantity -= 1;
+        if (state[index].quantity === 0) {
+          state.splice(index, 1);
+        }
+      }
+    },
   },
 });
 
 export const selectCart = (state) => state.cart;
 
-export const { addToCart } = cartSlice.actions;
+export const {
+  addToCart,
+  removeItemFromCart,
+  incrementQuantity,
+  decrementQuantity,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
