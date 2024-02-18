f99427128aaf439f847dd9a4b42c17fb4c34872e
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-18 15:33:04 +0330

    Commit

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -3,10 +3,29 @@ import React from 'react';
 import './cart.scss';
 import Image from 'next/image';
 import { useSelector, useDispatch } from 'react-redux';
-import { addToCart, selectCart } from '@/features/cart/cartSlice';
+import {
+  addToCart,
+  selectCart,
+  incrementQuantity,
+  decrementQuantity,
+  removeItemFromCart,
+} from '@/features/cart/cartSlice';
 
 const Cart = () => {
   const cart = useSelector(selectCart);
+  const dispatch = useDispatch();
+
+  const handleIncrement = (id) => {
+    dispatch(incrementQuantity(id));
+  };
+
+  const handleDecrement = (id) => {
+    dispatch(decrementQuantity(id));
+  };
+
+  const handleRemoveItem = (id) => {
+    dispatch(removeItemFromCart(id));
+  };
 
   return (
     <div className="cart">
@@ -23,19 +42,25 @@ const Cart = () => {
       <div className="cart-container">
         {cart.map((item) => (
           <div key={item.id} className="cart-item-box">
+            <button
+              className="remove-btn"
+              onClick={() => handleRemoveItem(item.id)}
+            >
+              Remove
+            </button>
             <div className="cart-image-container">
               <Image src={item.img} alt={item.title} className="cart-image" />
             </div>
             <div className="cart-content">
               <h3>{item.title}</h3>
-              <p>{item.content}</p>
+              <p>{item.content.substring(0, 199)}</p>
               <p className="cart-price">Price: ${item.price}</p>
             </div>
 
             <div className="cart-button-container">
-              <button>+</button>
+              <button onClick={() => handleIncrement(item.id)}>+</button>
               <span>{item.quantity}</span>
-              <button>-</button>
+              <button onClick={() => handleDecrement(item.id)}>-</button>
             </div>
           </div>
         ))}
